a5248f95839a6e1b348c871bdad16cd4467d6504
MRK-( MAX ) <113408834+MoonnGuy@users.noreply.github.com>
2023-05-02 22:52:11 -0700

    Update LinearSearch.cpp

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,1 +1,11 @@
-
+int LinearSearch(int numbers[], int numbersSize, int key) {
+   int i;
+   
+   for (i = 0; i < numbersSize; ++i) {
+      if (numbers[i] == key) {
+         return i;
+      }
+   }
+   
+   return -1; /* not found */
+}
